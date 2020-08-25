01bb2ee04abc68fa3ee915cdcdfc2febe2709cc6
FALL1N1 <emiliyan9604bg@gmail.com>
2020-08-25 12:27:03 +0300

    [Core/Scripts] Add the bastards to the ScriptLoader

--- src/server/scripts/Outland/outland_script_loader.cpp
+++ src/server/scripts/Outland/outland_script_loader.cpp
@@ -1,5 +1,24 @@
+void AddSC_instance_black_temple();
+void AddSC_boss_najentus();
+void AddSC_boss_supremus();
+void AddSC_boss_shade_of_akama();
+void AddSC_boss_gurtogg_bloodboil();
+void AddSC_boss_teron_gorefiend();
+void AddSC_boss_reliquary_of_souls();
+void AddSC_boss_mother_shahraz(); 
+void AddSC_boss_illidari_council();
+void AddSC_boss_illidan();
 
 void AddOutlandScripts()
 {
-	
+	AddSC_instance_black_temple();
+	AddSC_boss_najentus();
+	AddSC_boss_supremus();
+	AddSC_boss_shade_of_akama();
+	AddSC_boss_gurtogg_bloodboil();
+	AddSC_boss_teron_gorefiend();
+	AddSC_boss_reliquary_of_souls();
+	AddSC_boss_mother_shahraz();
+	AddSC_boss_illidari_council();
+	AddSC_boss_illidan();
 }
